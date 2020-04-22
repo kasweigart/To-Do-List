0f72430ae21a31f31fb33defb79917545869050d
Kyle Sweigart <kasweigart@gmail.com>
2020-04-22 17:05:49 -0400

    added bootstrap navbar and sidebar

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//FACTORY FUNCTION FOR CREATING TODOS
+function createToDo(title, desc, dueDate, priority) {
+    return {
+        title,
+        desc,
+        dueDate,
+        priority
+    };
+}
+
+let projects = [];
+
